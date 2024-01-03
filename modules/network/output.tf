4e6abea1372f410c18fa41c5d570b83401844a3b
gayucharvi <153450955+gayucharvi@users.noreply.github.com>
2024-01-03 22:07:56 +0530

    Update output.tf

--- modules/network/output.tf
+++ modules/network/output.tf
@@ -2,9 +2,3 @@
 output "List_of_AZs" {
   value = data.aws_availability_zones.available
 }
-output "private_subnets_id" {
-  value = aws_subnet.private_sb.id
-}
-output "public_subnets_id" {
-  value = aws_subnet.public_sb.id
-}
